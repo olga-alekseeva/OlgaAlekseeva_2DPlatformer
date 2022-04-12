19a75fd83d54722f7bb5c0b8f734f2c214e50005
olga-alekseeva <oliasca@gmail.com>
2022-04-12 16:30:30 +0300

    Монетки

---
+++ Assets/Scripts/Controllers/CoinsController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Platformer_2D
+{
+    public class CoinsController: IDisposable
+    {
+        private float _animationSpeed = 10f;
+        private SpriteAnimatorController _spriteAnimator;
+        private LevelObjectView _playerView;
+        private  List<LevelObjectView> _coinViews;
+
+        public CoinsController(LevelObjectView playerView, List<LevelObjectView> coinViews, SpriteAnimatorController spriteAnimator)
+        {
+            _playerView = playerView;
+            _coinViews = coinViews;
+            _spriteAnimator = spriteAnimator;
+
+            _playerView.OnLevelObjectContact += OnLevelObjectContact;
+            foreach (LevelObjectView coin in coinViews)
+            {
+                _spriteAnimator.StartAnimation(coin._spriteRenderer, AnimState.Idle, true, _animationSpeed);
+            }
+        }
+        
+            private void OnLevelObjectContact(LevelObjectView contactView)
+
+        {
+            if(_coinViews.Contains(contactView))
+            {
+                _spriteAnimator?.StopAnimation(contactView._spriteRenderer);
+               GameObject.Destroy(contactView.gameObject);
+            }
+
+        }
+        public void Dispose()
+        {
+            _playerView.OnLevelObjectContact -= OnLevelObjectContact;
+        }
+    
+    }
+}
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Platformer_2D
@@ -5,19 +6,25 @@ namespace Platformer_2D
     public class Main : MonoBehaviour
     {
         [SerializeField] private SpriteAnimatorConfig _playerConfig;
-        [SerializeField] private int _playerAnimationSpeed = 15;
-        [SerializeField] private LevelObjectView _playerView;
-        [SerializeField] private CannonView _cannonView;
+        [SerializeField] private SpriteAnimatorConfig _coinConfig;
         [SerializeField] private SpriteAnimatorConfig _enemyConfig;
+        [SerializeField] private int _playerAnimationSpeed = 15;
         [SerializeField] private int _enemyAnimationSpeed = 3;
+        [SerializeField] private LevelObjectView _playerView;
         [SerializeField] private LevelObjectView _enemyView;
+        [SerializeField] private CannonView _cannonView;
+        [SerializeField] private List<LevelObjectView> _coinViews;
+
+
 
         private SpriteAnimatorController _enemyAnimator;
         private SpriteAnimatorController _playerAnimator;
+        private SpriteAnimatorController _coinAnimator;
         private CannonAimController _cannonAimController;
         private BulletEmitterController _bulletEmitterController;
         private PlayerController _playerController;
         private CameraController _cameraController;
+        private CoinsController _coinsController;
        // private ParalaxController _paralaxController;
 
         private void Awake()
@@ -27,13 +34,16 @@ namespace Platformer_2D
             _playerAnimator.StartAnimation(_playerView._spriteRenderer, AnimState.Idle, true, _playerAnimationSpeed);
 
             _enemyConfig = Resources.Load<SpriteAnimatorConfig>("EnemyAnimCfg");
+            _coinConfig = Resources.Load<SpriteAnimatorConfig>("CoinAnimCfg");
             _enemyAnimator = new SpriteAnimatorController(_enemyConfig);
+            _coinAnimator = new SpriteAnimatorController(_coinConfig);
             _enemyAnimator.StartAnimation(_enemyView._spriteRenderer, AnimState.Idle, true, _enemyAnimationSpeed);
             _cannonAimController = new CannonAimController(_cannonView._muzzleTransform, _playerView._transform); 
             _bulletEmitterController = new BulletEmitterController(_cannonView._bullets, _cannonView._emitterTransform);
             _playerController = new PlayerController(_playerView, _playerAnimator);
             _cameraController = new CameraController(_playerView, Camera.main.transform);
-           // _paralaxController = new ParalaxController(_paralaxController._camera, _paralaxController._back);
+            _coinsController = new CoinsController(_playerView, _coinViews, _coinAnimator);
+            // _paralaxController = new ParalaxController(_paralaxController._camera, _paralaxController._back);
         }
 
         void Update()
@@ -44,6 +54,7 @@ namespace Platformer_2D
             _cameraController.Update();
             _cannonAimController.Update();
             _bulletEmitterController.Update();
+            _coinAnimator.Update();
          //   _paralaxController.Update();
         }
     }
