d4d5d3928763c91900fec54f22030d5a6adc41af
olga-alekseeva <oliasca@gmail.com>
2022-04-17 16:57:49 +0300

    Добавлен класс bulletView
    Пока ни на что не влияет

---
+++ Assets/Scripts/View/BulletView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+namespace Platformer_2D
+{
+    public class BulletView : LevelObjectView
+
+    {
+        [SerializeField] private TrailRenderer _trail;
+        public void SetVisible(bool visible)
+        {
+            if (_trail) _trail.enabled = visible;
+            if (_trail) _trail.Clear();
+            SpriteRenderer spriteRenderer = new SpriteRenderer();
+            spriteRenderer.enabled = visible;
+        }
+    }
+}
